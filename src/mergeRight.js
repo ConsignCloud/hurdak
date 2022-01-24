94c885e8003287fb9a03d61662e55822e5a1775a
Jonathan Staab <shtaab@gmail.com>
2022-01-24 13:27:08 -0800

    Make merge/mergeRight only 2 arity to avoid accidental merging of additional arguments

--- src/mergeRight.js
+++ src/mergeRight.js
@@ -1,11 +1,3 @@
 import curryN from './curryN'
 
-export default curryN(2, (...xs) => {
-  let r = {}
-
-  for (const x of xs) {
-    r = {...x, ...r}
-  }
-
-  return r
-})
+export default curryN(2, (a, b) => ({...b, ...a}))
--- test/mergeRight.test.js
+++ test/mergeRight.test.js
@@ -2,7 +2,7 @@ import {assert} from 'chai'
 import mergeRight from 'hurdak/mergeRight'
 
 describe('mergeRight', () => {
-  it('Merges two+ objects', () => {
-    assert.deepEqual(mergeRight({a: 1, b: 2}, {b: 3, c: 4}, {b: 4, d: 5}), {a: 1, b: 2, c: 4, d: 5})
+  it('Merges exactly two objects', () => {
+    assert.deepEqual(mergeRight({a: 1, b: 2}, {b: 3, c: 4}, {b: 4, d: 5}), {a: 1, b: 2, c: 4})
   })
 })
