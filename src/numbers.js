f222433a4fdd41b895c0c9b028ece38abcc32ebf
Jonathan Staab <shtaab@gmail.com>
2022-01-25 08:50:24 -0800

    Fix parseFloat

--- src/numbers.js
+++ src/numbers.js
@@ -1,12 +1,6 @@
-import is from 'ramda/src/is'
-
 const DOT_REGEXP = new RegExp(/\.+/)
 
 const _parseFloat = (x, fallback = 0) => {
-  if (is(String, x) && !isNaN(x)) {
-    return parseFloat(x)
-  }
-
   x = parseFloat(x.toString().replace(DOT_REGEXP, '.').trim())
 
   return isNaN(x) ? fallback : x
